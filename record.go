a15fbb7a108081b6d1f1c93d4abe6038f964a8c3
myc880628 <528290772@qq.com>
2018-06-24 20:39:59 +0800

    Create record.go

---
+++ record.go
@@ -0,0 +1,78 @@
+package log2
+
+import (
+	"time"
+)
+
+type (
+	Record interface {
+		Level() Level
+		Line() int
+		Time() time.Time
+		Stack([]byte) int
+		Prefix() string
+		Message() string
+		File() string
+		Func() string
+	}
+)
+
+type (
+	record struct {
+		t       time.Time
+		line    uint32
+		level   Level
+		prefix  string
+		fn      string
+		file    string
+		message string
+		pcs     []uintptr
+	}
+)
+
+func (r *record) Time() time.Time {
+	return r.t
+}
+
+func (r *record) Message() string {
+	return r.message
+}
+
+func (r *record) Prefix() string {
+	return r.prefix
+}
+
+func (r *record) Line() int {
+	return int(r.line)
+}
+
+func (r *record) File() string {
+	return r.file
+}
+
+func (r *record) Func() string {
+	return r.fn
+}
+
+func (r *record) Level() Level {
+	return r.level
+}
+
+func (r *record) Stack(buffer []byte) int {
+	if len(buffer) == 0 {
+		return 0
+	}
+	if len(r.pcs) == 0 {
+		return 0
+	}
+	return getStack(r.pcs, buffer)
+}
+
+func newRecord(level Level, message, prefix string) *record {
+	return &record{
+		level:   level,
+		t:       time.Now(),
+		message: message,
+		prefix:  prefix,
+	}
+}
