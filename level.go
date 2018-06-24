a911795380ebc4936c0f7c1ba0f71f3ae15bde33
myc880628 <528290772@qq.com>
2018-06-24 20:42:25 +0800

    Create level.go

---
+++ level.go
@@ -0,0 +1,33 @@
+package log2
+
+type (
+	Level uint32
+)
+
+const (
+	DebugLevel Level = iota
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+	PanicLevel
+	Disabled
+)
+
+func (l Level) String() string {
+	switch l {
+	case DebugLevel:
+		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarnLevel:
+		return "warn"
+	case ErrorLevel:
+		return "error"
+	case FatalLevel:
+		return "fatal"
+	case PanicLevel:
+		return "panic"
+	}
+	return ""
+}
